e72092ce609063f37afc0364bd981dd7563a9c52
meirson1 <noam220599@gmail.com>
2021-11-28 15:21:11 +0200

    StartWay2-fixed pc

---
+++ src/test/TimeSeries.java
@@ -0,0 +1,16 @@
+package test;
+
+//import java.util.HashMap;
+import java.util.Hashtable;
+
+public class TimeSeries {
+
+	Hashtable<> t=new Hashtable(k,v);
+
+	public TimeSeries(String csvFileName) {
+		//StringBuilder cvs= new StringBuilder(csvFileName);
+		csvFileName.split(",");
+		csvFileName
+	}
+	
+}
